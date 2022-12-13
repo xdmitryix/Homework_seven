7c106b5087010cbbdef3fe6c3863053a017d2dc5
dmitry <rbdeman@yandex.ru>
2022-12-13 22:33:08 +0300

    Поправил третью задачу

--- Zadacha_three/Program.cs
+++ Zadacha_three/Program.cs
@@ -7,9 +7,42 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+void ArithmeticMean(double[,] array)
 {
-    int[,] array = new int[rows, columns];
+    double sum = 0;
+    double result = 0;
+    int count = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+            count++;
+        }
+        result = sum / count;
+        Console.Write("{0:0.0}", result);
+        Console.Write("; ");
+        result = 0;
+        sum = 0;
+        count = 0;
+    }
+}
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    double[,] array = new double[rows, columns];
 
     for (int i = 0; i < rows; i++)
     {
@@ -22,43 +55,10 @@ int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
 }
 
 
-void ShowArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
-
-void ArithmeticMean(int[,] array)
-{
-    int sum = 0;
-    int result = 0;
-    int count = 0;
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            sum = sum + array[i, j];
-            count++;
-        }
-        result = sum / count;
-        Console.Write(result + ";");
-        result = 0;
-        sum = 0;
-        count = 0;
-    }
-}
-
-
 int rows = 3;
 int columns = 4;
-int[,] matrix = CreateRandomArray(rows, columns, 1, 100);
+double[,] matrix = CreateRandomArray(rows, columns, 1, 100);
 ShowArray(matrix);
 Console.WriteLine();
+Console.WriteLine("Среднее арифметическое каждого столбца:");
 ArithmeticMean(matrix);
