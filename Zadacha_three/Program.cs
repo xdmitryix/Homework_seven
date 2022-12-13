c6d4c6aa2f8397af98eaa838254eeecbd16a6ca1
dmitry <rbdeman@yandex.ru>
2022-12-13 21:57:23 +0300

    сделал третью задачу

--- Zadacha_three/Program.cs
+++ Zadacha_three/Program.cs
@@ -1,1 +1,64 @@
-﻿
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ArithmeticMean(int[,] array)
+{
+    int sum = 0;
+    int result = 0;
+    int count = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+            count++;
+        }
+        result = sum / count;
+        Console.Write(result + ";");
+        result = 0;
+        sum = 0;
+        count = 0;
+    }
+}
+
+
+int rows = 3;
+int columns = 4;
+int[,] matrix = CreateRandomArray(rows, columns, 1, 100);
+ShowArray(matrix);
+Console.WriteLine();
+ArithmeticMean(matrix);
