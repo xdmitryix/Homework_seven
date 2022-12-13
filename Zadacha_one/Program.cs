eab8f9e1195638fe6149aa7919925f1bc08e8ac4
dmitry <rbdeman@yandex.ru>
2022-12-13 19:44:14 +0300

    сделал первую задачу

--- Zadacha_one/Program.cs
+++ Zadacha_one/Program.cs
@@ -1,1 +1,60 @@
-﻿
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    double[,] array = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(rand.Next(leftRange, rightRange +1) + rand.NextDouble(), 1);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+
+int m = EnterNumber("введите значение m: ");
+int n = EnterNumber("введите значение n: ");
+double[,] matrix = CreateRandomArray(m, n, -10, 10);
+ShowArray(matrix);
+
+
+
+
+
+
+
+
+
+
+
+
